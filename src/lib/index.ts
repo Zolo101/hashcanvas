8e3472d2e4eba61427b3205494d7e5d4cd0b4099
Zolo101 <umadeguy@gmail.com>
2024-01-11 23:42:13 +0000

    Fix Firefox bug, bump to 0.4.0

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,5 @@
-import HashCanvas from "$lib/HashCanvas.svelte";
-import type WebGL2Renderer from "./render/webgl2/webgl2.js";
-import type WebGPURenderer from "./render/webgpu/webgpu.js";
+import WebGL2Renderer from "$lib/render/webgl2/webgl2.js";
+import WebGPURenderer from "$lib/render/webgpu/webgpu.js";
 
-export default HashCanvas
+export { WebGL2Renderer, WebGPURenderer }
 export type Renderer = WebGL2Renderer | WebGPURenderer;
--- src/lib/misc.ts
+++ src/lib/misc.ts
@@ -1,7 +1,15 @@
 export const isWebGL2Supported = () => {
-    const canvas = document.createElement("canvas");
+    const canvas = document.createElement("canvas")
     const gl = canvas.getContext("webgl2");
     return !!gl;
 }
 
-export const isWebGPUSupported = () => !!navigator.gpu;
+export const isWebGPUSupported = () => {
+    const canvas = document.createElement("canvas")
+    const hasWebGPUDevice = !!navigator.gpu;
+    if (hasWebGPUDevice) {
+        return !!canvas.getContext("webgpu");
+    } else {
+        return false;
+    }
+}
