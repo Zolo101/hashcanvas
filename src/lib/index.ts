e22da2b0e24837ad66be8c1017705936a7b653ef
Zolo101 <umadeguy@gmail.com>
2024-01-12 00:47:14 +0000

    Fix imports, bump to 0.5.0

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,7 @@
+import HashCanvas from "$lib/HashCanvas.svelte";
 import WebGL2Renderer from "$lib/render/webgl2/webgl2.js";
 import WebGPURenderer from "$lib/render/webgpu/webgpu.js";
 
+export default HashCanvas
 export { WebGL2Renderer, WebGPURenderer }
 export type Renderer = WebGL2Renderer | WebGPURenderer;
